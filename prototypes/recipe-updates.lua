a37ec6654c483a90831c45754f69add40b7933e8
sapphiriza <sapphiriza@gmail.com>
2025-06-08 12:17:18 -0700

    Update changelog and FDSL version

--- prototypes/recipe-updates.lua
+++ prototypes/recipe-updates.lua
@@ -1,17 +1,9 @@
-local function add_recipe_category(recipe_name, category_name)
-  local recipe = data.raw.recipe[recipe_name]
-  if recipe then
-    if not recipe.additional_categories then
-      recipe.additional_categories = {}
-    end
-    table.insert(recipe.additional_categories, category_name)
-  end
-end
+local frep = require("__fdsl__.lib.recipe")
 
 if settings.startup["astroponics-crude-oil"].value then
-  add_recipe_category("bioslurry-putrefaction", "fulgoran-cryogenics")
+  frep.add_category("bioslurry-putrefaction", "fulgoran-cryogenics")
 end
 
 if mods["bztin"] then
-  add_recipe_category("organotins", "fulgoran-cryogenics")
+  frep.add_category("organotins", "fulgoran-cryogenics")
 end
