1ea2a5b6aca33e04006d246eba7afdfc74964ddd
SafTheLamb <safthelamb@gmail.com>
2025-07-02 15:48:59 -0700

    Fix migration crash

--- migrations/migrations.lua
+++ migrations/migrations.lua
@@ -3,8 +3,12 @@ for _,force in pairs(game.forces) do
   local recipes = force.recipes
   local mods = script.active_mods
 
-  recipes["cerys-upgrade-fulgoran-moon-garden-quality"].enabled = technologies["cerys-fulgoran-machine-quality-upgrades"].researched
+  if technologies["cerys-advanced-structure-repair"] and technologies["cerys-advanced-structure-repair"].researched then
+    recipes["cerys-upgrade-fulgoran-moon-garden-quality"].enabled = true
+  end
   if mods["bztin"] then
-    recipes["alternative-nitrogen-rich-mineral-processing"].enabled = technologies["cerys-nitrogen-rich-mineral-processing"].researched
+    if technologies["cerys-nitrogen-rich-mineral-processing"] and technologies["cerys-nitrogen-rich-mineral-processing"].researched then
+      recipes["alternative-nitrogen-rich-mineral-processing"].enabled = true
+    end
   end
 end
