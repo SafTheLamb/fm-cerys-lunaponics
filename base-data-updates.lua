ba2174071d916babcd45df7d03af1b2fc8cdca69
Sapphira Riza <safthelamb@gmail.com>
2026-09-12 21:58:34 -0700

    2.1?

--- base-data-updates.lua
+++ base-data-updates.lua
@@ -6,7 +6,7 @@ frep.replace_ingredient("cerys-lubricant-synthesis", "light-oil", "bioslurry")
 
 if mods["bztin"] and data.raw.item["tinned-cable"] then
   frep.replace_result("cerys-nuclear-scrap-recycling", "copper-cable", "tinned-cable")
-  frep.scale_result("cerys-nuclear-scrap-recycling", "tinned-cable", {probability=2})
+  frep.scale_result("cerys-nuclear-scrap-recycling", "tinned-cable", {independent_probability=2})
 end
 
 ftech.add_unlock("cerys-fulgoran-machine-quality-upgrades", "cerys-upgrade-fulgoran-moon-garden-quality")
@@ -21,9 +21,9 @@ end
 local cerys = data.raw.planet["cerys"]
 cerys.map_gen_settings.autoplace_settings["entity"].settings["cerys-fulgoran-moon-garden"] = {}
 
-table.insert(data.raw["simple-entity"]["cerys-ruin-small"].minable.results, {type="item", name="tree-seed", amount=1, probability=0.12})
-table.insert(data.raw["simple-entity"]["cerys-ruin-medium"].minable.results, {type="item", name="tree-seed", amount=1, probability=0.12})
-table.insert(data.raw["simple-entity"]["cerys-ruin-big"].minable.results, {type="item", name="tree-seed", amount=1, probability=0.43})
+table.insert(data.raw["simple-entity"]["cerys-ruin-small"].minable.results, {type="item", name="tree-seed", amount=1, independent_probability=0.12})
+table.insert(data.raw["simple-entity"]["cerys-ruin-medium"].minable.results, {type="item", name="tree-seed", amount=1, independent_probability=0.12})
+table.insert(data.raw["simple-entity"]["cerys-ruin-big"].minable.results, {type="item", name="tree-seed", amount=1, independent_probability=0.43})
 table.insert(data.raw["simple-entity"]["cerys-ruin-huge"].minable.results, {type="item", name="tree-seed", amount=1})
 table.insert(data.raw["simple-entity"]["cerys-ruin-colossal"].minable.results, {type="item", name="tree-seed", amount=2})
 
@@ -34,8 +34,8 @@ table.insert(data.raw["simple-entity"]["cerys-ruin-big"].minable.results, {type=
 table.insert(data.raw["simple-entity"]["cerys-ruin-huge"].minable.results, {type="item", name=lumber_item, amount_min=18, amount_max=25})
 table.insert(data.raw["simple-entity"]["cerys-ruin-colossal"].minable.results, {type="item", name=lumber_item, amount_min=32, amount_max=39})
 
-table.insert(data.raw["simple-entity"]["cerys-ruin-small"].minable.results, {type="item", name="low-density-structure", amount=1, probability=0.12})
-table.insert(data.raw["simple-entity"]["cerys-ruin-medium"].minable.results, {type="item", name="low-density-structure", amount=1, probability=0.12})
+table.insert(data.raw["simple-entity"]["cerys-ruin-small"].minable.results, {type="item", name="low-density-structure", amount=1, independent_probability=0.12})
+table.insert(data.raw["simple-entity"]["cerys-ruin-medium"].minable.results, {type="item", name="low-density-structure", amount=1, independent_probability=0.12})
 table.insert(data.raw["simple-entity"]["cerys-ruin-big"].minable.results, {type="item", name="low-density-structure", amount=2})
 table.insert(data.raw["simple-entity"]["cerys-ruin-huge"].minable.results, {type="item", name="low-density-structure", amount=4})
 table.insert(data.raw["simple-entity"]["cerys-ruin-colossal"].minable.results, {type="item", name="low-density-structure", amount=7})
